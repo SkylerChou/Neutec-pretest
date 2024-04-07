299b9081974030ac52d7fce33a9e423fc98d9f3c
SkylerChou <kim19961012@gmail.com>
2024-04-07 13:02:43 +0800

    feat : 九宮格動畫完成

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="min-h-dvh flex justify-center items-center">
     <div class="container grid grid-rows-3 grid-cols-3 gap-4 p-4">
-      <div class="item h-[100px] border-2 border-black"></div>
-      <div class="item h-[100px] border-2 border-black"></div>
-      <div class="item h-[100px] border-2 border-black"></div>
-      <div class="item h-[100px] border-2 border-black"></div>
-      <div class="item h-[100px] border-2 border-black"></div>
-      <div class="item h-[100px] border-2 border-black"></div>
-      <div class="item h-[100px] border-2 border-black"></div>
-      <div class="item h-[100px] border-2 border-black"></div>
-      <div class="item h-[100px] border-2 border-black"></div>
+      <div class="relative item h-[100px] border-2 border-black bg-radial-gradient before:content-[''] before:">
+        <div
+          class="w-[30px] h-[30px] z-10 absolute top-0 left-0 bottom-0 right-0 m-auto bg-[#A5F12B] rounded-full animate-[slideInLeft_1s_infinite]"
+        ></div>
+      </div>
+      <div class="item h-[100px] border-2 border-black bg-radial-gradient"></div>
+      <div
+        class="item h-[100px] border-2 border-black bg-radial-gradient relative before:content-[''] before:absolute before:w-full before:h-full before:bg-white before:bg-opacity-30 before:animate-[blink_1s_infinite]"
+      >
+        <div
+          class="w-[30px] h-[30px] z-10 absolute top-0 left-0 bottom-0 right-0 m-auto bg-[#A5F12B] rounded-full animate-[slideInLeft_1s_infinite]"
+        ></div>
+      </div>
+      <div class="relative item h-[100px] border-2 border-black bg-radial-gradient"></div>
+      <div
+        class="item h-[100px] border-2 border-black bg-radial-gradient relative before:content-[''] before:absolute before:w-full before:h-full before:bg-white before:bg-opacity-30 before:animate-[blink_1s_infinite]"
+      ></div>
+      <div class="item h-[100px] border-2 border-black bg-radial-gradient"></div>
+      <div class="relative item h-[100px] border-2 border-black bg-radial-gradient">
+        <div
+          class="w-[30px] h-[30px] z-10 absolute top-0 left-0 bottom-0 right-0 m-auto bg-[#A5F12B] rounded-full animate-[slideInLeft_1s_infinite]"
+        ></div>
+      </div>
+      <div class="item h-[100px] border-2 border-black bg-radial-gradient"></div>
+      <div
+        class="item h-[100px] border-2 border-black bg-radial-gradient relative before:content-[''] before:absolute before:w-full before:h-full before:bg-white before:bg-opacity-30 before:animate-[blink_1s_infinite]"
+      >
+        <div
+          class="w-[30px] h-[30px] z-10 absolute top-0 left-0 bottom-0 right-0 m-auto bg-[#A5F12B] rounded-full animate-[slideInLeft_1s_infinite]"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,7 +39,4 @@
 <script setup></script>
 
 <style scoped>
-.item {
-  background: radial-gradient(circle, rgba(113, 81, 95, 1) 81%, rgba(0, 0, 0, 1) 100%);
-}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(circle, rgba(113, 81, 95, 1) 81%, rgba(0, 0, 0, 1) 100%);',
+      },
+      keyframes: {
+        blink: {
+          to: { opacity: 1 },
+          '50%': { opacity: 0 },
+          from: { opacity: 1 },
+        },
+        slideInLeft: {
+          '0%': { transform: ' translateX(-100%)' },
+          '100%': { transform: 'translateX(900%)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
